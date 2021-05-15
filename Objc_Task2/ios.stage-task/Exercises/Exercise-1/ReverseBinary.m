ca4c17bc06692b7c61b8867eaa7841dd55948594
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-15 22:15:15 +0200

    feat: implement logic of task_2_p1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,32 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    //to binary
+        NSMutableString * string = [[NSMutableString alloc] init];
+    
+        int spacing = pow(2, 3);
+        int width = (sizeof(n)) *spacing;
+        int binaryDigit = 0;
+        int integer = n;
+    
+        while(binaryDigit < width) {
+            binaryDigit++;
+            [string insertString:((integer & 1) ? @"1" : @"0")atIndex:0];
+            integer = integer >> 1;
+        }
+        
+    //reverse string
+        NSMutableString *reversedString = [NSMutableString string];
+        NSInteger charIndex = [string length];
+        while (charIndex > 0) {
+            charIndex--;
+            NSRange subStrRange = NSMakeRange(charIndex, 1);
+            [reversedString appendString:[string substringWithRange:subStrRange]];
+        }
+        
+    //to int/long
+        long v = strtol([reversedString UTF8String], NULL, 2);
+        
+    return v;
 }
