85019a411b58d9694c5562cee841bfe899590c95
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-16 00:42:33 +0200

    feat: implement logic of task_2_p2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    NSInteger counter = 0;
+    
+    for (NSNumber *n1 in array) {
+        for (NSNumber *n2 in array) {
+            if (([n1 intValue]-[n2 intValue]) == [number intValue]){
+                counter++;
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
