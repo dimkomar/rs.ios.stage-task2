28a4333a6df6bec5b8b4bdacd95ca3ee685f2f82
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-16 14:12:55 +0200

    feat: implement logic of task_2_p3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,27 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSMutableArray* array = [[NSMutableArray alloc] initWithObjects: @"0", @"0", @"0", @"0", nil];
+    
+    if ((numbersArray == nil) || numbersArray.count > 4 || numbersArray.count == 0){
+        return @"";
+    } else {
+        for (int i = 0; i < numbersArray.count; i++){
+            NSNumber* number = numbersArray[i];
+            if ([number intValue] >= 0 && [number intValue] <= 255){
+                [array replaceObjectAtIndex:i withObject:[number stringValue]];
+            } else if ([number intValue] > 255) {
+                return @"The numbers in the input array can be in the range from 0 to 255.";
+            } else if ([number intValue] < 0) {
+                return @"Negative numbers are not valid for input.";
+            }
+        }
+    }
+    
+    NSString *greeting = [array componentsJoinedByString:@"."];
+    
+    return greeting;
 }
 
 @end
