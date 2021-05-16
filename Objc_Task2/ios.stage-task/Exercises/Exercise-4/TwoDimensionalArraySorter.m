33492e991177e24be9f61c08caf8fe93c42bb751
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-16 17:18:30 +0200

    feat: implement logic of task_2_p4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,35 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+        
+    NSMutableArray* numbers = [NSMutableArray array];
+    NSMutableArray* strings = [NSMutableArray array];
+    
+    if (array == nil) {
+        return @[];
+    }
+    
+    for (NSArray* subarray in array) {
+        if ([subarray isKindOfClass:[NSArray class]]) {
+            for (NSArray *value in subarray) {
+                if ([value isKindOfClass:[NSNumber class]]) {
+                    [numbers addObject:value];
+                } else if ([value isKindOfClass:[NSString class]]){
+                    [strings addObject:value];
+                }
+            }
+        } else {
+            return @[];
+        }
+    }
+    
+    NSArray* sortedNumericalArray = [numbers sortedArrayUsingSelector:@selector(compare:)];
+    NSArray* sortedStringArray = [strings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    NSMutableArray *result = [NSMutableArray arrayWithArray:sortedNumericalArray];
+    [result addObjectsFromArray:sortedStringArray];
+
+    return result;
 }
 
 @end
