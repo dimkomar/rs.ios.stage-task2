53c97c33553ae961fb93b0a5c07de75a37089857
Dmitriy Komar <dmitriyko@playtika.com>
2021-05-17 00:05:04 +0200

    feat: implement logic of task_2_p2_v2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -6,9 +6,20 @@
     
     NSInteger counter = 0;
     
+    /*
     for (NSNumber *n1 in array) {
         for (NSNumber *n2 in array) {
             if (([n1 intValue]-[n2 intValue]) == [number intValue]){
+                counter++;
+            }
+        }
+    }
+    */
+    
+    //v2
+    for (int i = 0; i < array.count; i++) {
+        for (int k = i + 1; k < array.count; k++) {
+            if (abs(array[k].intValue - array[i].intValue) == number.intValue) {
                 counter++;
             }
         }
